3e0fc57426274de02fd1c6ff96a7a0f228f35f7a
Darlan <darlaneduardoguimaraes@gmail.com>
2023-05-02 21:09:30 -0300

    first commit and construct data base

---
+++ aplicacao/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Cardapio(models.Model):
+
+    nome = models.CharField(max_length=200, null=False, blank=False)
+    ingredientes = models.TextField(null=False, blank=False)
+    valor = models.FloatField(null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    status = models.BooleanField(default=False, blank=False)
+
+
+class Comanda(models.Model):
+
+    qrcode = models.TextField(null=False, blank=False)
+    caminho = models.CharField(max_length=100, null=False, blank=False)
+    total = models.FloatField(null=True, blank=False)
+
+
+class Mesa(models.Model):
+
+    qrcode = models.TextField(null=False, blank=False)
+    caminho = models.CharField(max_length=100, null=False, blank=False)
+
+
+class Pedido(models.Model):
+    cardapio = models.ForeignKey(Cardapio, on_delete=models.CASCADE)
+    comanda = models.ForeignKey(Comanda, on_delete=models.CASCADE)
+    mesa = models.ForeignKey(Mesa, on_delete=models.CASCADE)
+    status = models.TextField(max_length=100, null=False, blank=False)
+
+
+
+
